1eed05142477d7023b73215f411715702484adf4
Ostrowskii <vanessaostroski08@gmail.com>
2023-05-29 23:28:46 -0400

    login added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Depends
 
-from app.model import PostSchema
+from app.model import PostSchema, UserSchema, UserLoginSchema
+from app.auth.auth_bearer import JWTBearer
+from app.auth.auth_handler import signJWT
 
 posts = [
     {
@@ -25,6 +27,14 @@ users = []
 
 
 app = FastAPI()
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
 
 # get - for testing
 @app.get("/", tags=["test"])
@@ -59,4 +69,19 @@ def add_post(post: PostSchema):
     posts.append(post.dict())
     return {
         "info":"Post Added!"
+    }
+
+
+@app.post("/user/signup", tags=["user"])
+def create_user(user: UserSchema = Body(...)):
+    users.append(user) # replace with db call, making sure to hash the password first
+    return signJWT(user.email)
+
+
+@app.post("/user/login", tags=["user"])
+def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
     }
